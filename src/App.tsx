e16c91816e7be3399fd09e05332593b7cd458333
박신우 <myhappyman@naver.com>
2024-02-02 09:01:41 +0900

    [Feat]: 프로젝트 설정 및 저스턴드 예제 테스트
    -prettier설정
    -zustand설정
    -기본예제 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-
+import { BearState, useBearStore } from './zustand/firstStore.ts';
 
 function App() {
-
+  const bears = useBearStore((state: BearState) => state && state.bears);
+  const increasePopulation = useBearStore((state: BearState) => state.increasePopulation);
+  const initBear = useBearStore((state: BearState) => state.removeAllBears);
   return (
     <div>
-        <p>TEST</p>
+      <div>
+        <p>현재 곰 숫자: {bears}</p>
+      </div>
+      <div>
+        <button onClick={increasePopulation}>늘어나라</button>
+        <button onClick={initBear}>초기화</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
