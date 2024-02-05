a761a82783ac33e53e1757600a493332744bfbf8
박신우 <myhappyman@naver.com>
2024-02-05 17:38:01 +0900

    [Feat]: zustand 학습중
    -메소드뿐만 아니라 setState를 통해서도 강제로 다른 컴포넌트에서도 별도의 훅을 작성할 수 있다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { BearState, useBearStore } from './zustand/firstStore.ts';
+import { create } from 'zustand';
 
 function App() {
   const bears = useBearStore((state: BearState) => state && state.bears);
   const increasePopulation = useBearStore((state: BearState) => state.increasePopulation);
   const initBear = useBearStore((state: BearState) => state.removeAllBears);
+
+  const { count } = useGlobalTest();
   return (
     <div>
       <div>
@@ -13,8 +16,27 @@ function App() {
         <button onClick={increasePopulation}>늘어나라</button>
         <button onClick={initBear}>초기화</button>
       </div>
+
+      <button onClick={() => useGlobalTest.setState({ count: count + 1 })}>+증가</button>
+      <Test />
     </div>
   );
 }
 
 export default App;
+
+interface useGlobalTestProps {
+  count: number;
+}
+const useGlobalTest = create<useGlobalTestProps>(() => ({
+  count: 0,
+}));
+
+function Test() {
+  const { count } = useGlobalTest();
+  return (
+    <div>
+      <p>{count}</p>
+    </div>
+  );
+}
